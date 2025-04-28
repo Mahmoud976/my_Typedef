51736a783308b03fcec81fc5acd5b68c58a738e8
Mahmoud976 <74363008+Mahmoud976@users.noreply.github.com>
2025-04-28 02:16:03 +0200

    Update my_typedef.h

--- my_typedef.h
+++ my_typedef.h
@@ -2,14 +2,14 @@
 #define my_typedef
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-//typedef unsigned long uint32_t;
+//typedef unsigned int uint32_t;
+typedef unsigned long uint32_t;
 typedef unsigned long long int uint64_t;
 
 typedef signed char sint8_t;
 typedef signed short sint16_t;
-typedef signed int sint32_t;
-//typedef signed long sint32_t;
+//typedef signed int sint32_t;
+typedef signed long sint32_t;
 typedef signed long long int sint64_t;
 #endif my_typedef// !1
 
