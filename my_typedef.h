5f29b22578d7b4e9bb4405a925f1e7adc13f9dc0
Mahmoud Saleh <mahmoud.saleh.2372001@gmail.com>
2025-04-28 03:12:09 +0200

    Created type alias for better readability

---
+++ my_typedef.h
@@ -0,0 +1,15 @@
+#ifndef my_typedef
+#define my_typedef
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+//typedef unsigned long uint32_t;
+typedef unsigned long long int uint64_t;
+
+typedef signed char sint8_t;
+typedef signed short sint16_t;
+typedef signed int sint32_t;
+//typedef signed long sint32_t;
+typedef signed long long int sint64_t;
+#endif my_typedef// !1
+
